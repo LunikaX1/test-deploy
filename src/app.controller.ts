643bef3c80deba4dd2b0e5bbe41da85439eb686f
ThaeN <thaenpong@gmail.com>
2025-10-03 10:25:20 +0700

    add test path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("test")
+  test():string{
+    return this.appService.tet();
+  }
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World! My name is dreamm and this is the testing the auto deploy of docker';
   }
+
+  test():string{
+    return "this is test the docker recreate"
+  }
 }
